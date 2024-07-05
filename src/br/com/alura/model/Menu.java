77e7b0e7cfcdc5adaf1f75d30a577f2fb12f9536
Andre Venditti <am7venditti@gmail.com>
2024-07-05 17:05:12 -0300

    Adding class 'Menu'

---
+++ src/br/com/alura/model/Menu.java
@@ -0,0 +1,17 @@
+package br.com.alura.model;
+
+public class Menu {
+    public void CriacaoMenu() {
+        System.out.println("*************************************");
+        System.out.println("Seja vem-vindo ao conversor de moedas: \n");
+        System.out.println("1) Dolar =>> Peso argentino");
+        System.out.println("2) Peso argentino =>> Dolar");
+        System.out.println("3) Dolar =>> Real Brasileiro");
+        System.out.println("4) Real brasileiro =>> Dolar");
+        System.out.println("5) Dolar =>> Peso colombiano");
+        System.out.println("6) Peso colombiano =>> Dolar");
+        System.out.println("7) Sair\n");
+        System.out.println("Escolha uma opção válida:");
+        System.out.println("*************************************\n\n");
+    }
+}
